0f200042268348f8b380d9f6150bd54735fb16e8
trt <trt@TRT>
2016-12-29 15:29:01 +0530

    Changes in database configuration.

--- util/connections/connection.js
+++ util/connections/connection.js
@@ -11,7 +11,8 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
 	if(err){
-		throw err;
+            console.log(err);
+            throw err;                
 	}
 });
 
